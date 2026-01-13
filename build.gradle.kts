91817ba512ed1200af62da1b2f379e3ce293ef1a
Fredrik Welander <76183002+welandfr@users.noreply.github.com>
2026-01-13 19:40:39 +0200

    Update JUnit version and add UTF-8 encoding

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.13.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
