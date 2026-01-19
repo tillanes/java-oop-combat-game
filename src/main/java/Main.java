98b9632c0e6666596688b6561575465fd45b1968
tillanes <tillanes@arcada.fi>
2026-01-19 19:01:57 +0200

    1.b 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Let the Battle begin");
+        GameCharacter dude = new GameCharacter("dude",100);
+        GameCharacter ghoul = new GameCharacter("Ghoul",50);
+
+        System.out.println("A scary-looking Ghoul runs towards you. You decide to attack.");
+
+        while (true){
+
+            ghoul.takeDamage(20);
+            dude.takeDamage(20);
+            if (ghoul.getHitPoints() < 0 ) break;
+
+            System.out.println("Dude hits ghoul for 20 HP. Ghoul has " + ghoul.getHitPoints() + " left");
+            System.out.println("Ghoul hits dude for 20 HP. Dude has " + dude.getHitPoints() + " left");
+
+
+        }
+        System.out.println("Ghoul is dead");
+        System.out.println("Dude wins!");
+
     }
 }
